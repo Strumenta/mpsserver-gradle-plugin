1cd038583a09f6a95da5c74023f6968e4552c462
Federico Tomassetti <federico@tomassetti.me>
2020-05-17 10:48:43 +0200

    initial commit

---
+++ src/main/groovy/com/strumenta/mpsserver/MpsServerGradlePlugin.groovy
@@ -0,0 +1,9 @@
+class MpsServerGradlePlugin implements Plugin<Project> {
+    void apply(Project project) {
+        project.task('hello') {
+            doLast {
+                println 'Hello from the GreetingPlugin'
+            }
+        }
+    }
+}
